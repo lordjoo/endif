13f92774b6fe489964d966d4547fd948511f5d52
lordjoo <youssef.afify122@gmail.com>
2021-01-06 22:22:31 +0200

    اللهم ابطل السحر يارب

--- logic/content.js
+++ logic/content.js
@@ -6,11 +6,12 @@ var db = firebase.firestore();
         window.location.assign("/notfound")
     }
     let c = course.docs[0].data();
+    let c_id = course.docs[0].id;
     $("#c_name").text(c.name)
     $("#c_photo").css("background-image",`url('${c.photo}')`);
-    $("#c_add_lesson").attr("href","/admin/addLesson/"+c.id)
+    $("#c_add_lesson").attr("href","/admin/addLesson/"+c_id)
     $("#c_add_lesson").removeClass("disabled");
-    db.collection("lessons").where("course_id","==",c.id)
+    db.collection("lessons").where("course_id","==",c_id)
         .onSnapshot(function (snap) {
             
         });
