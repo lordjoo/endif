21a6d5c862e041a37292944c8795921a2662ee87
lordjoo <youssef.afify122@gmail.com>
2021-01-06 17:28:04 +0200

    add lesson

--- logic/content.js
+++ logic/content.js
@@ -8,6 +8,7 @@ var db = firebase.firestore();
     let c = course.docs[0].data();
     $("#c_name").text(c.name)
     $("#c_photo").css("background-image",`url('${c.photo}')`);
+    $("#c_add_lesson").attr("href","/admin/addLesson/"+course.docs[0].id);
     db.collection("lessons").where("course_id","==",course.docs[0].id)
         .onSnapshot(function (snap) {
             
